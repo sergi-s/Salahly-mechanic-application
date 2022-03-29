e6158e3524f92f27378d18fdcddf3bab60597d79
Aya Adel <67385952+AyaAdel-dev@users.noreply.github.com>
2022-03-29 13:54:37 +0200

    set mechanic available

---
+++ lib/screens/homepage/testscreen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:salahly_mechanic/classes/firebase/customgeofire.dart';
+
+class SetAvalability extends StatelessWidget {
+
+  SetAvalability({Key? key}) : super(key: key);
+  static final routeName = "/setavalabilityscreen";
+  NearbyLocations as=NearbyLocations();
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: ToggleSwitch(
+          minWidth: 90.0,
+          initialLabelIndex: 1,
+          cornerRadius: 20.0,
+          activeFgColor: Colors.white,
+          inactiveBgColor: Colors.grey,
+          inactiveFgColor: Colors.white,
+          totalSwitches: 2,
+          labels: ['', ''],
+          icons: [FontAwesomeIcons.faceSadCry, FontAwesomeIcons.faceKissWinkHeart],
+          activeBgColors: [[Colors.red],[Colors.green]],
+          onToggle: (index) async{
+            if(index==0){
+            await NearbyLocations.deleteNearbyMechanicsAndProviders();
+            }
+            if(index==1){
+              await NearbyLocations.addNearbyMechanicsAndProviders();
+            }
+
+          },
+        ),
+      ),
+    );
+  }
+}
